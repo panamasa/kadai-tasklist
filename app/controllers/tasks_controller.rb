be633ea79c23a645bc3da2a3022090e113b92081
katsumasa.suwa <kt.suwa1023@gmail.com>
2021-09-24 08:56:36 +0000

    3回目の提出の修正後

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    if logged_in?
-      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
-    end
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
   end
   
   def show
